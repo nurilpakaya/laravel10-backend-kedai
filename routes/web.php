a49463d70536d5bbab5d9ed940ea141435c893a3
nurilpakaya <nurilpakaya@gmail.com>
2024-11-07 10:43:14 +0700

    product

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
 
 Route::get('/', function () {
     return view('pages.auth.auth-login');
-   
+
 });
 
 // Route::get('/dashboard', function () {
@@ -16,4 +18,6 @@ Route::middleware(['auth'])->group(function () {
     Route::get('home', function () {
         return view('pages.dashboard', ['type_menu' => 'home']);
     })->name('home');
+
+    Route::resource('user', UserController::class);
 });
